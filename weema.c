ec1a6221d8d8bbacd262b53b0191e26a57166f41
Henrique Gogó <henriquegogo@gmail.com>
2019-08-02 01:03:00 -0300

    More keyboard control for window position

--- weema.c
+++ weema.c
@@ -15,42 +15,63 @@ int main() {
     XButtonEvent start;
     XEvent ev;
 
+    KeyCode tab_key, up_key, left_key, right_key, f4_key, del_key;
+
     if (!(dpy = XOpenDisplay(0x0))) return 1;
 
     root = DefaultRootWindow(dpy);
     XSelectInput(dpy, root, SubstructureNotifyMask);
 
-    // Intercept keys and mouse buttons. Mod1Mask = Alt. LockMask = CapsLock. Mod2Mask = NumLock
-    unsigned int modifiers[4] = { Mod1Mask, Mod1Mask|LockMask, Mod1Mask|Mod2Mask, Mod1Mask|Mod2Mask|LockMask };
-    for (int i = 0; i < 4; i++) {
-        XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("Tab")), modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
-        XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F4")),  modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
-        XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F11")), modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
-        XGrabButton(dpy, 1, modifiers[i], root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
-        XGrabButton(dpy, 2, modifiers[i], root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
-        XGrabButton(dpy, 3, modifiers[i], root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+    // Intercept keys and mouse buttons. Mod2Mask = NumLock, Mod3Mask = ScrollLock, LockMask = CapsLock
+    unsigned int modifiers[8] = { None, Mod2Mask, Mod3Mask, LockMask, Mod2Mask|Mod3Mask, Mod2Mask|LockMask, Mod3Mask|LockMask, Mod2Mask|Mod3Mask|LockMask };
+    for (int i = 0; i < 8; i++) {
+        XGrabKey(dpy, tab_key   = XKeysymToKeycode(dpy, XStringToKeysym("Tab")),   Mod1Mask|modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabKey(dpy, up_key    = XKeysymToKeycode(dpy, XStringToKeysym("Up")),    Mod4Mask|modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabKey(dpy, left_key  = XKeysymToKeycode(dpy, XStringToKeysym("Left")),  Mod4Mask|modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabKey(dpy, right_key = XKeysymToKeycode(dpy, XStringToKeysym("Right")), Mod4Mask|modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabKey(dpy, f4_key    = XKeysymToKeycode(dpy, XStringToKeysym("F4")),    Mod1Mask|modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabKey(dpy, del_key   = XKeysymToKeycode(dpy, XStringToKeysym("Del")),   ControlMask|Mod1Mask|modifiers[i], root, True, GrabModeAsync, GrabModeAsync);
+        XGrabButton(dpy, 1, Mod1Mask|modifiers[i], root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+        XGrabButton(dpy, 2, Mod1Mask|modifiers[i], root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+        XGrabButton(dpy, 3, Mod1Mask|modifiers[i], root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
     }
 
     for(;;) {
         XNextEvent(dpy, &ev);
         // Keyboard keypress
-        if (ev.type == KeyPress && ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("Tab"))) {
-            XCirculateSubwindowsDown(dpy, root);
-        }
-        else if (ev.type == KeyPress && ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("F4")) && ev.xkey.subwindow) {
-            XEvent event;
-            event.xclient.type = ClientMessage;
-            event.xclient.window = ev.xkey.subwindow;
-            event.xclient.message_type = XInternAtom(dpy, "WM_PROTOCOLS", True);
-            event.xclient.format = 32;
-            event.xclient.data.l[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
-            event.xclient.data.l[1] = CurrentTime;
-            XSendEvent(dpy, ev.xkey.subwindow, False, NoEventMask, &event);
-        }
-        else if (ev.type == KeyPress && ev.xkey.keycode == XKeysymToKeycode(dpy, XStringToKeysym("F11")) && ev.xkey.subwindow) {
-            XGetWindowAttributes(dpy, root, &attr);
-            XMoveWindow(dpy, ev.xkey.subwindow, 0, 0);
-            XResizeWindow(dpy, ev.xkey.subwindow, attr.width, attr.height);
+        if (ev.type == KeyPress) {
+            if (ev.xkey.keycode == tab_key) {
+                XCirculateSubwindowsDown(dpy, root);
+            }
+            if (ev.xkey.subwindow != None) {
+                if (ev.xkey.keycode == up_key) {
+                    XGetWindowAttributes(dpy, root, &attr);
+                    XMoveWindow(dpy, ev.xkey.subwindow, 0, 0);
+                    XResizeWindow(dpy, ev.xkey.subwindow, attr.width, attr.height);
+                }
+                else if (ev.xkey.keycode == left_key) {
+                    XGetWindowAttributes(dpy, root, &attr);
+                    XMoveWindow(dpy, ev.xkey.subwindow, 0, 0);
+                    XResizeWindow(dpy, ev.xkey.subwindow, attr.width / 2, attr.height);
+                }
+                else if (ev.xkey.keycode == right_key) {
+                    XGetWindowAttributes(dpy, root, &attr);
+                    XMoveWindow(dpy, ev.xkey.subwindow, attr.width / 2, 0);
+                    XResizeWindow(dpy, ev.xkey.subwindow, attr.width / 2, attr.height);
+                }
+                else if (ev.xkey.keycode == del_key) {
+                }
+                else if (ev.xkey.keycode == f4_key) {
+                    XEvent event;
+                    event.xclient.type = ClientMessage;
+                    event.xclient.window = ev.xkey.subwindow;
+                    event.xclient.message_type = XInternAtom(dpy, "WM_PROTOCOLS", True);
+                    event.xclient.format = 32;
+                    event.xclient.data.l[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
+                    event.xclient.data.l[1] = CurrentTime;
+                    XSendEvent(dpy, ev.xkey.subwindow, False, NoEventMask, &event);
+                }
+            }
         }
         // Mouse clicks
         else if (ev.type == ButtonPress && ev.xbutton.subwindow != None) {
