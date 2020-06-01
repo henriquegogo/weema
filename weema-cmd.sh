50b6a30a3ba7fb128a19f3b11b3b094cc686231a
Henrique Gogó <henriquegogo@gmail.com>
2020-05-31 23:50:29 -0300

    Rewriting all code to remove XSetInput, Raise and Mouse actions

--- weema-cmd.sh
+++ weema-cmd.sh
@@ -1,5 +1,7 @@
 if [ $1 = "launcher" ]; then
   dmenu_run
+elif [ $1 = "terminal" ]; then
+  x-terminal-emulator &
 elif [ $1 = "volumeup" ]; then
   amixer set Master 3+
 elif [ $1 = "volumedown" ]; then
