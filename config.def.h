da701981995d8f2c48254eb816624e9f5695ecfc
Henrique Gogó <henriquegogo@gmail.com>
2021-09-16 02:03:06 -0300

    Config array memory redimensioning

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 // One-letter shortcuts will be binded 'Super-Letter'.
-const char CMD_KEYS[][24][128] = {
+const char CMD_KEYS[][24][32] = {
   {"XF86AudioRaiseVolume", "amixer set Master 3+"},
   {"XF86AudioLowerVolume", "amixer set Master 3-"},
   {"Print", "scrot"},
