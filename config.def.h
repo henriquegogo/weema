c67a484c995d6dc014252a071a6fc211f6720140
Henrique Gogó <henriquegogo@gmail.com>
2022-12-20 17:37:38 -0300

    Add .weemarc example. Add xrandr as dependencies

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 // One-letter shortcuts will be binded 'Super-Letter'.
 const char CMD_KEYS[][24][32] = {
-  {"XF86AudioRaiseVolume", "amixer set Master 3+"},
-  {"XF86AudioLowerVolume", "amixer set Master 3-"},
-  {"Print", "scrot"},
+  {"XF86AudioRaiseVolume", "amixer set Master 3%+"},
+  {"XF86AudioLowerVolume", "amixer set Master 3%-"},
+  {"Print", "scrot -s"},
   {"r", "dmenu_run"},
   {"t", "x-terminal-emulator &"},
   {"b", "x-www-browser &"},
