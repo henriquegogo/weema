5cafd6573f36848bc57a754d3361bbb830a70ba3
Henrique Soares <henriquegogo@gmail.com>
2022-12-21 22:41:39 -0300

    Changing default volume change

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 // One-letter shortcuts will be binded 'Super-Letter'.
 const char CMD_KEYS[][24][32] = {
-  {"XF86AudioRaiseVolume", "amixer set Master 3%+"},
-  {"XF86AudioLowerVolume", "amixer set Master 3%-"},
+  {"XF86AudioRaiseVolume", "amixer set Master 5%+"},
+  {"XF86AudioLowerVolume", "amixer set Master 5%-"},
   {"Print", "scrot -s"},
   {"r", "dmenu_run"},
   {"t", "x-terminal-emulator &"},
