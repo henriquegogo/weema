4c0b6669ae7065adfe366b144bed1abd7ac9372c
Henrique Soares <henriquegogo@gmail.com>
2022-12-28 01:45:05 -0300

    Remove window list

--- config.def.h
+++ config.def.h
@@ -6,6 +6,5 @@ const char CMD_KEYS[][24][128] = {
   {"r", "dmenu_run"},
   {"t", "x-terminal-emulator &"},
   {"b", "x-www-browser &"},
-  {"l", "slock"},
-  {"1", "xdotool search --onlyvisible --name \"$(xdotool search --onlyvisible --name \".{1,}\" getwindowname %@ | dmenu -l 5)\" windowfocus"}
+  {"l", "slock"}
 };
