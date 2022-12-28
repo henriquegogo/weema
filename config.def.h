5aefcf5b2c85e932323a22992c7d80a41925f502
Henrique Gogó <henriquegogo@gmail.com>
2022-12-28 01:32:00 -0300

    Window selector shortcut

--- config.def.h
+++ config.def.h
@@ -1,10 +1,11 @@
 // One-letter shortcuts will be binded 'Super-Letter'.
-const char CMD_KEYS[][24][32] = {
+const char CMD_KEYS[][24][128] = {
   {"XF86AudioRaiseVolume", "amixer set Master 5%+"},
   {"XF86AudioLowerVolume", "amixer set Master 5%-"},
   {"Print", "scrot -s"},
   {"r", "dmenu_run"},
   {"t", "x-terminal-emulator &"},
   {"b", "x-www-browser &"},
-  {"l", "slock"}
+  {"l", "slock"},
+  {"1", "xdotool search --onlyvisible --name \"$(xdotool search --onlyvisible --name \".{1,}\" getwindowname %@ | dmenu -l 5)\" windowfocus"}
 };
