0c88e33945b74b2ef6e3c8632e12a455a10b98d9
Henrique Gogó <henriquegogo@gmail.com>
2024-11-27 12:26:35 -0300

    Startup commands at config.h. Remove .weemarc

--- config.def.h
+++ config.def.h
@@ -2,9 +2,17 @@
 const char CMD_KEYS[][24][128] = {
   {"XF86AudioRaiseVolume", "amixer set Master 5%+"},
   {"XF86AudioLowerVolume", "amixer set Master 5%-"},
-  {"Print", "scrot -s"},
+  {"Print", "scrot -s -e 'xclip -selection clipboard -t image/png -i $f'"},
   {"r", "dmenu_run"},
   {"t", "x-terminal-emulator &"},
   {"b", "x-www-browser &"},
   {"l", "slock"}
+};
+
+const char STARTUP_CMDS[][128] = {
+  /*
+  "xsetroot -solid '#223'",
+  "xrandr --output HDMI-1-0 --auto --left-of eDP-1",
+  "xcompmgr &"
+  */
 };
