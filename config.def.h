72a2977fb3efe37986f317c069a0b36e93045311
Henrique Gogó <henriquegogo@gmail.com>
2025-08-13 23:41:27 -0300

    Add brightness control shortcuts

--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 // One-letter shortcuts will be binded 'Super-Letter'.
 const char CMD_KEYS[][24][128] = {
+  {"XF86MonBrightnessUp", "brightnessctl set 10%+"},
+  {"XF86MonBrightnessDown", "brightnessctl set 10%-"},
   {"XF86AudioRaiseVolume", "amixer set Master 5%+"},
   {"XF86AudioLowerVolume", "amixer set Master 5%-"},
   {"equal", "amixer set Master 5%+"},
